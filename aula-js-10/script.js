e5743b03159650b8d77032b6d8fb981d66bd631e
DevThiagoCBM <thiago999011@gmail.com>
2026-06-29 20:13:15 -0300

    Create script.js

---
+++ aula-js-10/script.js
@@ -0,0 +1,33 @@
+//se a hora tiver entre as 6 e as 12 bom dia
+// se for entere 12 e 18 boa tarde
+//caso contrario: boa noite
+let hora = 10;
+if (hora > 6 && hora < 12) {
+    console.log('bom dia');
+} else if (hora > 12 && hora < 18) {
+        console.log('boa tarde');
+    } else {
+            console.log('boa noite');
+};
+//switch... case -->
+let permissao = 'comum'; // comum, gerente, diretor
+switch (permissao) {
+    case 'comum':
+        console.log('usuario comum');
+        break;
+    case 'gerente':
+        console.log('usuario gerente');
+        break;
+    case 'diretor':
+        console.log('usuario gerente');
+        break;
+};
+// Laço de repetição --->
+for (let i = 0; i < 5; i++) { //repetição com laço for
+    console.log('Estou Aprendendo!');
+}
+for (let i = 0; i <= 5; i++) { //numeros impares entre 0 e 5
+    if (i % 2 !== 0) {
+        console.log(i);
+    };
+};
