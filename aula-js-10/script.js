03c113881f7518de2df448cd66d0563337504860
DevThiagoCBM <thiago999011@gmail.com>
2026-07-08 09:33:08 -0300

    Update script.js

--- aula-js-10/script.js
+++ aula-js-10/script.js
@@ -23,9 +23,10 @@ switch (permissao) {
         break;
 };
 // Laço de repetição --->
+//for
 for (let i = 0; i < 5; i++) { //repetição com laço for
     console.log('Estou Aprendendo!');
-}
+};
 for (let i = 0; i <= 5; i++) { //numeros impares entre 0 e 5
     if (i % 2 !== 0) {
         console.log(i);
@@ -38,5 +39,61 @@ while (ii >= 1) {
         console.log(ii);
     };
     ii--;
-}
+};
 //do while
+let i = 1;
+do {
+    console.log('Olá!', i);
+    i++;
+} while (i <= 10);
+// for in //para arrays
+const pessoa = {
+    nome: 'Jhonatan',
+    idade: 25
+};
+for (let key in pessoa) {
+    console.log(key, pessoa[key]); //posso usar o proprio key pra expor as propriedades do objeto pessoa.
+};
+const cores = ['vermelho', 'azul', 'verde'];
+for (let indice in cores) {
+    console.log(indice, cores[indice]);
+};
+//for of
+for (let cor of cores) { //o cor aqui é o valor contido em cada indice percorrido durante o loop
+    console.log(cor);
+};
+let valorMaior = maiorNumero(5, 9);
+console.log(valorMaior);
+function maiorNumero (n1, n2) {
+    if (n1 > n2) {
+        return n1;
+    } else {
+        return n2;
+    };
+};
+//se divisivel por 3 => fizz
+//se por 5 => buzz
+//pelos dois => fizzbuzz
+//por nenhum => retorna o mesmo valor
+//se nao for um numero =. nao é um numero
+
+let resultado = fizzBuzz(5)
+console.log(resultado)
+
+function fizzBuzz (entrada) {
+    if (entrada === NaN) {
+        return 'Isso não é um número!';
+    };
+    if ((entrada % 3 === 0) && (entrada % 5 === 0)) {
+        return 'fizzBuzz'
+    };
+    if (entrada % 3  === 0) {
+        return 'fizz';
+    };
+    if (entrada % 5 === 0) {
+        return 'buzz';
+    };
+    if ((entrada % 3 !== 0) || (entrada % 5 !== 0)) {
+        return entrada;
+    };
+}; 
